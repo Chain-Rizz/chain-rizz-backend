c0cf5242373c94eafa3bb1006ee1b8f71c3ade41
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-10-12 19:00:36 +0100

    Swagger Integrated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Stellar x CCTP')
+    .setDescription('API Documentation for Circled Fiat')
+    .setVersion('1.0')
+    .addTag('Stellar x CCTP')
+    .addBearerAuth(
+      {
+        description: `Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
